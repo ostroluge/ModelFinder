bfbaf80560b8ff8994ecd9cb2039eddef8a1ee85
Julien Flamen <flamen.julien@gmail.com>
2016-02-26 00:26:52 +0100

    Passage à Spring boot

---
+++ ModelFinder/src/main/java/org/acteacademie/modelfinder/ModelFinderApplication.java
@@ -0,0 +1,12 @@
+package org.acteacademie.modelfinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelFinderApplication.class, args);
+	}
+}

36c7ed845c892f8ae91ff6f0e7919014f6931e84
Thomas <ostrowski.thomas@gmail.com>
2016-03-01 23:31:05 +0100

    add getters and setters

--- ModelFinder/src/main/java/org/acteacademie/modelfinder/model/Admin.java
+++ ModelFinder/src/main/java/org/acteacademie/modelfinder/model/Admin.java
@@ -18,5 +18,36 @@ public class Admin {
 		this.firstname = firstname;
 		this.email = email;
 	}
-	
+
+	public Long getIdAdmin() {
+		return idAdmin;
+	}
+
+	public void setIdAdmin(Long idAdmin) {
+		this.idAdmin = idAdmin;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 }

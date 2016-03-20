15c09e10befe20e081d23f65183f8d811f4177ef
Thomas <ostrowski.thomas@gmail.com>
2016-03-20 16:13:21 +0100

    merge from testPOST

--- ModelFinder/src/main/java/org/acteacademie/modelfinder/controllers/AdminController.java
+++ ModelFinder/src/main/java/org/acteacademie/modelfinder/controllers/AdminController.java
@@ -5,9 +5,13 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.acteacademie.modelfinder.domain.Admin;
+import org.acteacademie.modelfinder.domain.StringResponse;
 import org.acteacademie.modelfinder.repositories.AdminRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,5 +24,26 @@ public class AdminController {
 	@RequestMapping("/adminList")
 	public List<Admin> getAll(){
 		return this.adminRepository.findAll();
+	}
+	
+	@CrossOrigin
+	@RequestMapping(value="/login", method=RequestMethod.POST, produces = "application/json")
+	public @ResponseBody StringResponse loginSubmit(@RequestBody Admin admin) {
+		StringResponse response;
+		if (isAuthorized(admin)) {
+			response = new StringResponse("success");
+		} else {
+			response = new StringResponse("fail");
+		}
+		return response;
+	}
+
+	private boolean isAuthorized(Admin adminToCheck) {
+		for (Admin admin : adminRepository.findAll()) {
+			if (admin.getMail().equals(adminToCheck.getMail())) {
+				return true;
+			}
+		}
+		return false;
 	}
 }

ea8f943dd62d997d87d9fbc6ea6bfe780c066f94
LJUMEL <leo.jumel@gmail.com>
2016-04-16 23:12:32 +0200

    suggestion et detail modeles

--- ModelFinder/src/main/java/org/acteacademie/modelfinder/repositories/ModelRepository.java
+++ ModelFinder/src/main/java/org/acteacademie/modelfinder/repositories/ModelRepository.java
@@ -3,6 +3,9 @@ package org.acteacademie.modelfinder.repositories;
 import java.util.Collection;
 
 import org.acteacademie.modelfinder.domain.Model;
+import org.acteacademie.modelfinder.enums.EyeColorEnum;
+import org.acteacademie.modelfinder.enums.LengthHairEnum;
+import org.acteacademie.modelfinder.enums.SkinToneEnum;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ModelRepository extends CrudRepository<Model, Long>{
@@ -10,4 +13,9 @@ public interface ModelRepository extends CrudRepository<Model, Long>{
 	Collection<Model> findAll();
 	
 	Model findOne(Long id);
+	
+	Collection<Model> findBySkinToneAndHairColorAndEyeColorAndLengthHairAndHeightBetween(SkinToneEnum skinTone,
+			String hairColor, EyeColorEnum eyeColor, LengthHairEnum lengthHair, long height_min, long height_max);
+
+	Model findById(long id);
 }

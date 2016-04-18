bb65a03c1a4476d40181aa283b9d454db14c923a
Thomas <ostrowski.thomas@gmail.com>
2016-04-18 23:48:14 +0200

    add apply form

--- ModelFinder/src/main/java/org/acteacademie/modelfinder/controllers/ReponseController.java
+++ ModelFinder/src/main/java/org/acteacademie/modelfinder/controllers/ReponseController.java
@@ -4,10 +4,17 @@ import java.util.Collection;
 
 import javax.annotation.Resource;
 
+import org.acteacademie.modelfinder.domain.ApplyForm;
 import org.acteacademie.modelfinder.domain.Reponse;
+import org.acteacademie.modelfinder.domain.StringResponse;
+import org.acteacademie.modelfinder.services.AnnonceService;
+import org.acteacademie.modelfinder.services.ModelService;
 import org.acteacademie.modelfinder.services.ReponseService;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,12 +22,52 @@ public class ReponseController {
 	
 	@Resource
 	ReponseService reponseService;
+	@Resource
+	ModelService modelService;
+	@Resource 
+	AnnonceService annonceService;
 	
 	@CrossOrigin
 	@RequestMapping("/reponseList")
 	public Collection<Reponse> getAll(){
 		return this.reponseService.getAllReponse();
 	}
-	
+
+	@CrossOrigin
+	@RequestMapping(value="/apply", method=RequestMethod.POST, produces = "application/json")
+	public @ResponseBody StringResponse apply(@RequestBody ApplyForm applyForm) {
+		saveReponse(applyForm);
+		StringResponse response = new StringResponse("success");
+		return response;
+	}
+
+	private void saveReponse(ApplyForm applyForm) {
+		Reponse reponse = new Reponse();
+		if (applyForm.getComment() != null && !applyForm.getComment().equals("")) {
+			reponse.setComment(applyForm.getComment());
+		}
+		if (applyForm.getIdModel() != 0) {
+			reponse.setModel(modelService.getOneModel(Long.valueOf(applyForm.getIdModel())));
+		}
+		if (applyForm.getIdAnnonce() != null && !applyForm.getIdAnnonce().equals("")) {
+			reponse.setAnnonce(annonceService.getOneAnnonce(Long.valueOf(applyForm.getIdAnnonce())));
+		}
+		if (applyForm.getAccessory1() != null) {
+			reponse.setStatusAccessory1(applyForm.getAccessory1());
+		}
+		if (applyForm.getAccessory2() != null) {
+			reponse.setStatusAccessory2(applyForm.getAccessory2());
+		}
+		if (applyForm.getAccessory3() != null) {
+			reponse.setStatusAccessory3(applyForm.getAccessory3());
+		}
+		if (applyForm.getAccessory4() != null) {
+			reponse.setStatusAccessory4(applyForm.getAccessory4());
+		}
+		if (applyForm.getAccessory5() != null) {
+			reponse.setStatusAccessory5(applyForm.getAccessory5());
+		}
+		reponseService.saveReponse(reponse);
+	}
 }
 
---
+++ ModelFinder/src/main/java/org/acteacademie/modelfinder/domain/ApplyForm.java
@@ -0,0 +1,88 @@
+package org.acteacademie.modelfinder.domain;
+
+public class ApplyForm {
+
+	public int idModel;
+	public String idAnnonce;
+	public String comment;
+	public String accessory1;
+	public String accessory2;
+	public String accessory3;
+	public String accessory4;
+	public String accessory5;
+
+	public ApplyForm() {
+
+	}
+	
+	public int getIdModel() {
+		return idModel;
+	}
+	
+	public void setIdModel(int idModel) {
+		this.idModel = idModel;
+	}
+	
+	public String getIdAnnonce() {
+		return idAnnonce;
+	}
+	
+	public void setIdAnnonce(String idAnnonce) {
+		this.idAnnonce = idAnnonce;
+	}
+	
+	public String getComment() {
+		return comment;
+	}
+	
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+	
+	public String getAccessory1() {
+		return accessory1;
+	}
+	
+	public void setAccessory1(String accessory1) {
+		this.accessory1 = accessory1;
+	}
+	
+	public String getAccessory2() {
+		return accessory2;
+	}
+	
+	public void setAccessory2(String accessory2) {
+		this.accessory2 = accessory2;
+	}
+	
+	public String getAccessory3() {
+		return accessory3;
+	}
+	
+	public void setAccessory3(String accessory3) {
+		this.accessory3 = accessory3;
+	}
+	
+	public String getAccessory4() {
+		return accessory4;
+	}
+	
+	public void setAccessory4(String accessory4) {
+		this.accessory4 = accessory4;
+	}
+	
+	public String getAccessory5() {
+		return accessory5;
+	}
+	
+	public void setAccessory5(String accessory5) {
+		this.accessory5 = accessory5;
+	}
+
+	@Override
+	public String toString() {
+		return "ApplyForm [idModel=" + idModel + ", idAnnonce=" + idAnnonce + ", comment=" + comment + ", accessory1="
+				+ accessory1 + ", accessory2=" + accessory2 + ", accessory3=" + accessory3 + ", accessory4="
+				+ accessory4 + ", accessory5=" + accessory5 + "]";
+	}
+}

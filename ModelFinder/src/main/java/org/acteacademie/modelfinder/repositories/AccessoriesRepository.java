51dfaa3282238e0385776d074af5babc261d4bd7
FlorianeGb <fl.goubel@gmail.com>
2016-05-25 00:32:03 +0200

    fin reponse + recherche

--- ModelFinder/src/main/java/org/acteacademie/modelfinder/repositories/AccessoriesRepository.java
+++ ModelFinder/src/main/java/org/acteacademie/modelfinder/repositories/AccessoriesRepository.java
@@ -1,9 +1,14 @@
 package org.acteacademie.modelfinder.repositories;
 
+import java.util.Collection;
+
 import org.acteacademie.modelfinder.domain.Accessories;
 import org.springframework.data.repository.CrudRepository;
 
 public interface AccessoriesRepository  extends CrudRepository<Accessories, Long>{
 
 	Accessories findOne(Long id);
+	
+	Collection<Accessories> findAll();
+
 }
